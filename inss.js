bcdd896eeaadb26f1310a1ee02867e8bdae31176
brunoo250718 <brunoo_85@hotmail.com>
2026-04-17 20:15:35 -0300

    Versão inicial INSS

---
+++ inss.js
@@ -0,0 +1,29 @@
+function calcsalario() {
+
+    let sbruto = document.getElementById("sbruto").value;
+
+    if (sbruto <= 1621) {
+        let vdesconto = 7.5 / 100 * sbruto;
+        let sliquido = sbruto - vdesconto;
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("sliquido").value = sliquido.toFixed(2);
+    }
+    else if(sbruto <= 2902.84)  { 
+        let vdesconto = 9 / 100 * sbruto;
+        let sliquido = sbruto - vdesconto;
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("sliquido").value = sliquido.toFixed(2);
+    }
+    else if(sbruto <= 4354.27){
+        let vdesconto = 12 / 100 * sbruto;
+        let sliquido = sbruto - vdesconto;
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("sliquido").value = sliquido.toFixed(2);
+    }
+    else{
+        let vdesconto = 14 / 100 * sbruto;
+        let sliquido = sbruto - vdesconto;
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("sliquido").value = sliquido.toFixed(2);
+        }
+}
