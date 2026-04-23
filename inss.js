27f160ab43b9d75c9710da955c11f3caa7c67479
brunoo250718 <brunoo_85@hotmail.com>
2026-04-23 14:23:55 -0300

    Update inss.js
    Mudança anual de salário

--- inss.js
+++ inss.js
@@ -2,7 +2,7 @@ function calcsalario() {
 
     let sbruto = document.getElementById("sbruto").value;
 
-    if (sbruto <= 1621.18) {
+    if (sbruto <= 2508.25) {
         let vdesconto = 7.5 / 100 * sbruto;
         let sliquido = sbruto - vdesconto;
         document.getElementById("vdesconto").value = vdesconto.toFixed(2);
